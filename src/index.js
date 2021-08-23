903051c97857b8c43909b43dc3b678ff23662aab
Tatyana Levchenko <grib.tanya06@mail.ru>
2021-08-23 22:53:31 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    // write your solution here 
 
+    strTenSymbols = expr.split(/(.{10})/).filter(O=>O).join(',');                                                                                //Текущее регулярное выражение создает пустые элементы массива между кусками. filter(x=>x) используется для фильтрации этих пустых элементов
+    const regex = /10/ig;
+    const regex1 = /11/ig;
+    const regex2 = /00/ig;
+    const regex3 = /\*{10}/ig;
+    let newStr = strTenSymbols.replace(regex,'.').replace(regex1,'-').replace(regex2,'').replace(regex3,' ').split(',');
+    
+    let resArr = [];
+    for (let i in newStr){
+     // resArr += MORSE_TABLE[newStr[i]];
+     resArr += newStr[i] == ' ' ? ' ' : MORSE_TABLE[newStr[i]];
+    }
+    return resArr;
+}
 module.exports = {
     decode
 }
+
